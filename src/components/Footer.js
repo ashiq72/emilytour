08e7392a7b5c45f2a0e3737d4b70d9b9e9ebff71
ashiq72 <ahmedashik18k@gmail.com>
2024-04-27 22:36:29 +0600

    Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,47 @@
 import React from "react";
-import './Footerstyle.module.css'
+import "./Footerstyle.module.css";
 
 function Foter() {
-    return (
-        <footer>
-        <div className="footer-container d-flex justify-content-around">
-            <div className="contact-us">
-            <h3>Contact Us</h3>
-            <ul>
-                <li>123 Main St.</li>
-                <li>Anytown, USA 12345</li>
-                <li>Email: info@tripverse.com</li>
-                <li>Phone: 555-555-5555</li>
-            </ul>
-            </div>
-            <div className="company">
-            <h3>Company</h3>
-            <ul>
-                <li>About Us</li>
-                <li>Terms of Service</li>
-                <li>Privacy Policy</li>
-            </ul>
-            </div>
-            <div className="other-links">
-            <h3>Other Links</h3>
-            <ul>
-                <li>Blog</li>
-                <li>FAQ</li>
-                <li>Careers</li>
-            </ul>
-            </div>
+  return (
+    <footer>
+      <div className="footer-container d-flex justify-content-around">
+        <div className="contact-us">
+          <h3>Contact Us</h3>
+          <ul>
+            <li>Cai Lậy, </li>
+            <li>Tien Giang, Vietnam</li>
+            <li>Email: admin@emilytour.com</li>
+            <li>Phone: +84 93 439 91 75</li>
+          </ul>
         </div>
-        <p className="text-center">&copy; 2023 Trip Verse. All Rights Reserved.</p>
-        </footer>
-    );
-    }
-    
-    
-    const Footer = () => {
-        return (
-            <div>                 
-              <Foter />
-            </div>
-          );
-    };
-    
+        <div className="company">
+          <h3>Company</h3>
+          <ul>
+            <li>About Us</li>
+            <li>Terms of Service</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="other-links">
+          <h3>Other Links</h3>
+          <ul>
+            <li>Blog</li>
+            <li>FAQ</li>
+            <li>Careers</li>
+          </ul>
+        </div>
+      </div>
+      <p className="text-center">&copy; 2024 EmilyTour. All Rights Reserved.</p>
+    </footer>
+  );
+}
 
+const Footer = () => {
+  return (
+    <div>
+      <Foter />
+    </div>
+  );
+};
 
 export default Footer;
